92ab29d4ec247291cf426b80401059a8c2abbf28
lehoangduy23092005@gmail.com <lehoangduy23092005@gmail.com>
2025-06-01 15:02:32 +0700

    push alert DAO

--- src/java/dao/AlertDAO.java
+++ src/java/dao/AlertDAO.java
@@ -25,14 +25,25 @@ public class AlertDAO {
     private final String CREATE_ALERT = "INSERT INTO tblAlerts(userID, ticker, threshold, direction) VALUES(?, ?, ?, ?)";
     private final String UPDATE_ALERT = "UPDATE tblAlerts SET threshold = ?, status = ? WHERE alertID = ? AND userID = ? AND status = 'inactive'";
     private final String DELETE_ALERT = "DELETE FROM tblAlerts WHERE alertID = ? AND userID = ? AND status = 'inactive'";
+    private final String GET_ALL_ALERT = "SELECT * FROM tblAlerts";
+    private final String GET_ALERT_BY_ID = "SELECT * FROM tblAlerts WHERE AlertID = ?";
+    
+    public Alert getAlertByID(String alertID) throws SQLException{
+        try(Connection conn = DBContext.getConnection();
+                PreparedStatement stm = conn.prepareStatement(GET_ALERT_BY_ID)){
+            try(ResultSet rs = stm.executeQuery()){
+                while(rs.next()){
+                    return new Alert(rs.getInt("alertID"), rs.getString("UserID"), rs.getString("ticker"), rs.getFloat("threshold"), rs.getString("direction"), rs.getString("status"));
+                }
+            }
+        }
+        return null;
+    }
+    public ArrayList<Alert> getAllAlert() throws SQLException {
+        ArrayList<Alert> resultList = new ArrayList<>();
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(GET_ALL_ALERT);) {
 
-    public ArrayList<Alert> searchByTicker(String search) throws SQLException {
-        ArrayList<Alert> resultList = new ArrayList<>();
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_TICKER);
-                stm.setString(1, search );
-                ResultSet rs = stm.executeQuery();
+            try ( ResultSet rs = stm.executeQuery()) {
                 while (rs.next()) {
                     resultList.add(new Alert(rs.getInt("alertID"), rs.getString("userID"), rs.getString("ticker"),
                             rs.getFloat("threshold"), rs.getString("direction"), rs.getString("status")));
@@ -41,14 +52,12 @@ public class AlertDAO {
         }
         return resultList;
     }
-    
-    public ArrayList<Alert> searchByDirection(String search) throws SQLException {
+
+    public ArrayList<Alert> searchByTicker(String ticker) throws SQLException {
         ArrayList<Alert> resultList = new ArrayList<>();
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_DIRECTION);
-                stm.setString(1,  search );
-                ResultSet rs = stm.executeQuery();
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_TICKER);) {
+            stm.setString(1, ticker);
+            try ( ResultSet rs = stm.executeQuery()) {
                 while (rs.next()) {
                     resultList.add(new Alert(rs.getInt("alertID"), rs.getString("userID"), rs.getString("ticker"),
                             rs.getFloat("threshold"), rs.getString("direction"), rs.getString("status")));
@@ -57,13 +66,12 @@ public class AlertDAO {
         }
         return resultList;
     }
-    public ArrayList<Alert> searchByStatus(String search) throws SQLException {
+
+    public ArrayList<Alert> searchByDirection(String direction) throws SQLException {
         ArrayList<Alert> resultList = new ArrayList<>();
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_STATUS);
-                stm.setString(1,  search);
-                ResultSet rs = stm.executeQuery();
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_DIRECTION)) {
+            stm.setString(1, direction);
+            try ( ResultSet rs = stm.executeQuery()) {
                 while (rs.next()) {
                     resultList.add(new Alert(rs.getInt("alertID"), rs.getString("userID"), rs.getString("ticker"),
                             rs.getFloat("threshold"), rs.getString("direction"), rs.getString("status")));
@@ -72,47 +80,54 @@ public class AlertDAO {
         }
         return resultList;
     }
-    
-    public boolean create(Alert alert) throws Exception {
+
+    public ArrayList<Alert> searchByStatus(String status) throws SQLException {
+        ArrayList<Alert> resultList = new ArrayList<>();
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(SEARCH_ALERT_BY_STATUS);) {
+            stm.setString(1, status);
+            try ( ResultSet rs = stm.executeQuery();) {
+                while (rs.next()) {
+                    resultList.add(new Alert(rs.getInt("alertID"), rs.getString("userID"), rs.getString("ticker"),
+                            rs.getFloat("threshold"), rs.getString("direction"), rs.getString("status")));
+                }
+            }
+        }
+        return resultList;
+    }
+
+    public boolean create(String userID, String ticker, float threshold, String direction) throws SQLException {
+
         boolean isCreated = false;
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(CREATE_ALERT);
-                stm.setString(1, alert.getUserID());
-                stm.setString(2, alert.getTicker());
-                stm.setFloat(3, alert.getThreshold());
-                stm.setString(4, alert.getDirection());
-                isCreated = stm.executeUpdate() > 0;
-            }
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(CREATE_ALERT);) {
+            stm.setString(1, userID);
+            stm.setString(2, ticker);
+            stm.setFloat(3, threshold);
+            stm.setString(4, direction);
+            isCreated = stm.executeUpdate() > 0;
         }
         return isCreated;
     }
 
-    public boolean update(int alertID, String userID, float threshold, String status) throws Exception {
+    public boolean update(int alertID, String userID, float threshold, String status) throws SQLException {
 
         boolean isUpdated = false;
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(UPDATE_ALERT);
-                stm.setFloat(1, threshold);
-                stm.setString(2, status);
-                stm.setInt(3, alertID);
-                stm.setString(4, userID);
-                isUpdated = stm.executeUpdate() > 0;
-            }
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(UPDATE_ALERT);) {
+            stm.setFloat(1, threshold);
+            stm.setString(2, status);
+            stm.setInt(3, alertID);
+            stm.setString(4, userID);
+            isUpdated = stm.executeUpdate() > 0;
+
         }
         return isUpdated;
     }
 
-    public boolean delete(int alertID, String userID) throws Exception {
+    public boolean delete(int alertID, String userID) throws SQLException {
         boolean isDeleted = false;
-        try ( Connection conn = DBContext.getConnection()) {
-            if (conn != null) {
-                PreparedStatement stm = conn.prepareStatement(DELETE_ALERT);
-                stm.setInt(1, alertID);
-                stm.setString(2, userID);
-                isDeleted = stm.executeUpdate() > 0;
-            }
+        try ( Connection conn = DBContext.getConnection();  PreparedStatement stm = conn.prepareStatement(DELETE_ALERT);) {
+            stm.setInt(1, alertID);
+            stm.setString(2, userID);
+            isDeleted = stm.executeUpdate() > 0;
         }
         return isDeleted;
     }
